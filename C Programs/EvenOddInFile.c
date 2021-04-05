41e5f0527bc479e99612e8c83a4150802483a456
Ravindra Kesapure <77103847+RavindraKesapure@users.noreply.github.com>
2021-04-05 08:11:13 +0530

    Update EvenOddInFile.c

--- C Programs/EvenOddInFile.c
+++ C Programs/EvenOddInFile.c
@@ -1,34 +1,20 @@
 #include<stdio.h>
-#include<stdlib.h>
+
 void main()
 {
-	FILE *fp;
-	int ch;
-//	int count=0,vow=0,con=0;
-	fp=fopen("E:\\data2.txt","r");
 	
-	 int i = 1;
-
- // fscanf (fp, "%d", &i);    
-  while (!feof (fp))
-    {  
-      fscanf (fp, "%d", &i);  
-      
-      
-      if(i%2==0)
-      {
-      	printf("\n Even %d",i);
-	  }
-	  else
-	  {
-	   printf("\n odd %d",i);
-	  }
-      
-      
-      
-      
-	 // printf ("\n %d ", i);    
-    }
+   int n;
+   printf("Enter the number \n");
+   scanf("%d",&n);
+	
+   if(n%2==0)
+   {
+	    printf("%d is Even number ",n);
+   }
+   else
+   {
+	   printf("%d is Odd number ",n);
+   }
  
-	fclose(fp);
+	
 }
