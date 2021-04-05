10f215370c7b0ae04d68b5df1f13015ac445739b
Ravindra Kesapure <77103847+RavindraKesapure@users.noreply.github.com>
2021-04-05 08:17:55 +0530

    Update LeafYearOrNot.c

--- C Programs/LeafYearOrNot.c
+++ C Programs/LeafYearOrNot.c
@@ -2,28 +2,18 @@
 
 void main()
 {
-	 int n,i,count=0;
-	 printf("Enter the number\n");
-	 scanf("%d",&n);
-	 
-	 
-     for(i=2;i<n;i++)
-     {
-     	if(n%i==0)
-     	{
-     		count=1;
-     		break;
-		}
-	 }
-	 if(count==0)
-	 {
-	 	printf("%d is prime number ",n);
-	 }
-	 else
-	 {
-	 	printf("%d is not prime number ",n);
-	 }
+	int n;
+	printf("Enter the number\n");
+	scanf("%d",&n);
 	
+	if(n%4==0)
+	{
+		 printf("%d is Leaf year ",n);
+	}
+	else
+	{
+		printf("%d i not leaf year ",n);
+	}
 
 	 
 }
