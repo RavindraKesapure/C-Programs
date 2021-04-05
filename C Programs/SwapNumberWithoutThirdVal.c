48b1d9606fe2bea004f1662875388c7d2647e397
Ravindra Kesapure <77103847+RavindraKesapure@users.noreply.github.com>
2021-04-05 08:26:50 +0530

    Update SwapNumberWithoutThirdVal.c

--- C Programs/SwapNumberWithoutThirdVal.c
+++ C Programs/SwapNumberWithoutThirdVal.c
@@ -2,12 +2,16 @@
 
 void main()
 {
-	 int n;
+	  int a,b;
 	 printf("Enter the two number\n");
-	 scanf("%d",&n);
-	 
-	 
-	 printf("a --> %d\n",(n*n));
+	 scanf("%d",&a);
+	 scanf("%d",&b);
+	
 
+	 a =a+b;
+	 b =a-b;
+	 a =a-b;
 	 
+	 printf("a --> %d\n",a);
+	 printf("b --> %d",b);
 }
